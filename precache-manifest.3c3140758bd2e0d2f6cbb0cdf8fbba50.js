9863285bb3a8d3dc8713d771d81cc21b974581f2
vpidkivka <vpidkivka@eliteit.org>
2020-05-24 23:51:49 +0300

    Updates

---
+++ precache-manifest.3c3140758bd2e0d2f6cbb0cdf8fbba50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42870b207275d4645d23bc261ef8bef1",
+    "url": "/codete/index.html"
+  },
+  {
+    "revision": "acd36f77e6c2613f5305",
+    "url": "/codete/static/css/main.1e5f7975.chunk.css"
+  },
+  {
+    "revision": "b8966169a67ae45a3434",
+    "url": "/codete/static/js/2.00deb8bf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/codete/static/js/2.00deb8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd36f77e6c2613f5305",
+    "url": "/codete/static/js/main.927c1eed.chunk.js"
+  },
+  {
+    "revision": "403e53ad91b28084c0ed",
+    "url": "/codete/static/js/runtime-main.f9443d88.js"
+  }
+]);
